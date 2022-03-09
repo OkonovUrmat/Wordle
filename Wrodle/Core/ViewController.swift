3cb02647e37956cbe6cf9c3fad5bb755d7aedf00
OkonovUrmat <okonov.okonov@gmail.com>
2022-03-09 10:11:56 +0600

    ViewController: Setup constraints

--- Wrodle/Core/ViewController.swift
+++ Wrodle/Core/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    // UI
+    // Keyboard
+    // Game board
+    // Orange/Green
+    
+    let keyboardVC = KeyboardVC()
+    let boardVC = BoardVC()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemGray6
+        
+        addChildren()
     }
 
+    private func addChildren() {
+        addChild(keyboardVC)
+        keyboardVC.didMove(toParent: self)
+        keyboardVC.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(keyboardVC.view)
+        
+        addChild(boardVC)
+        boardVC.didMove(toParent: self)
+        boardVC.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(boardVC.view)
+        
+        addConstraints()
+    }
 
+    private func addConstraints() {
+        boardVC.view.snp.makeConstraints { make in
+            make.left.right.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.bottom.equalTo(keyboardVC.view.snp.top)
+            make.height.equalToSuperview().multipliedBy(0.6)
+        }
+        
+        keyboardVC.view.snp.makeConstraints { make in
+            make.left.right.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
+    
 }
 
